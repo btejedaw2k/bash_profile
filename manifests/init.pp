3da00a51c76bd636795f720c75eb7384166ac471
Bryan Tejeda <btejeda@sanservices.hnm>
2018-05-24 11:47:32 -0600

    fixing variable types best practice

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,8 +17,8 @@
 #     Default: 0644
 
 class bash_profile (
-  Enum['present', 'absent']   $file_ensure,
-  Optional[String]            $file_template,
+  Enum['present', 'absent']   $file_ensure      = 'present',
+  Optional[String]            $file_template    = 'profile_template.erb',
   String                      $file_parent_name = '/etc/profile',
   String                      $file_directory   = '/etc/profile.d',
   Optional[Hash]              $config_files,
