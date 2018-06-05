d292cecf0a975400a0ce1af87abc8645cfa197fe
Bryan Tejeda <btejeda@sanservices.hnm>
2018-06-05 09:14:02 -0600

    add example to module bash_profile

--- examples/calldefine.pp
+++ examples/calldefine.pp
@@ -1,3 +1,8 @@
-class { 'bash_profile::config':
-  source => '/tmp/test1/testing'
+include bash_profile
+
+bash_profile::config { 'profile1.sh':
+  source => '/tmp/test1/testing',
+  file_ensure => 'present',
+  file_directory => '/etc/profile.d',
+  file_parent_name => '/etc/profile',
 }
