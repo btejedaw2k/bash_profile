d5ed86d3628cb5a2fda89ee65cbb89d3015e3c70
Bryan Tejeda <btejeda@sanservices.hnm>
2018-06-05 10:23:24 -0600

    fixing parameter for init example

--- examples/init.pp
+++ examples/init.pp
@@ -1,8 +1,6 @@
-config_files = {
-  'profile1.sh' => {'source' => '/tmp/test1/testing' },
-  'testaccount' => { 'source' => '/tmp/test1/testaccount', 'account' => 'testaccount', 'account_dir' => '/home/testaccount'},
-}
-
 class { 'bash_profile':
-  config_files => $config_files
+  config_files => {
+    'profile1.sh' => {'source' => '/tmp/test1/testing' },
+    'testaccount' => { 'source' => '/tmp/test1/testaccount', 'account' => 'testaccount', 'account_dir' => '/home/testaccount'},
+  }
 }
