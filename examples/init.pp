4f0ac60e8005c8b8606db361dd7236f5121a7ec0
jamc96 <jamc.mejia@gmail.com>
2018-07-09 10:17:13 -0600

    puppet-syntax and linter issues fixed

--- examples/init.pp
+++ examples/init.pp
@@ -1,6 +1,8 @@
-class { 'bash_profile':
-  config_files => {
+# testing example
+$config_files = {
     'profile1.sh' => {'source' => '/tmp/test1/testing' },
-    'test' => { 'source' => '/tmp/test1/testing2', 'account' => 'testuser', 'account_dir' => '/home/testuser'},
+    'test'        => {'source' => '/tmp/test1/testing2', 'account' => 'testuser', 'account_dir' => '/home/testuser' },
   }
+class { 'bash_profile':
+  config_files => $config_files,
 }
--- spec/defines/config_spec.rb
+++ spec/defines/config_spec.rb
@@ -28,6 +28,6 @@ describe 'bash_profile::config' do
     it { is_expected.to compile }
     it { is_expected.to compile.with_all_deps }
 
-    it { is_expected. to contain_file('/var/lib/foo').with( ensure: 'present', source: '/tmp/test') }
+    it { is_expected. to contain_file('/var/lib/foo').with(ensure: 'present', source: '/tmp/test') }
   end
 end
